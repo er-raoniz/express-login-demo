a8e7faf87dfc982248680b2923ce14e9cefe531b
Rahul Soni <rahulvsoni1997@gmail.com>
2021-07-05 12:11:05 +0530

    add mysql service

---
+++ config/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    "mysql": {
+        "host": "us-cdbr-east-04.cleardb.com",
+        "database": "heroku_a7729c5b9079347",
+        "username": "b8494c5a7e6053",
+        "password": "365639cb"
+    }
+}
---
+++ services/mysqlService.js
@@ -0,0 +1,74 @@
+const mysql = require('mysql');
+
+const mysqlConfig = {
+    host: env.mysql.host,
+    user: env.mysql.username,
+    password: env.mysql.password,
+    database: env.mysql.database
+}
+
+const pool = mysql.createPool(mysqlConfig);
+
+function connect(cb) {
+    return new Promise((resolve, reject) => {
+        pool.on('connection', function (connection) {
+            connection.on('error', function (err) {
+                console.error('MySQL error event', err);
+            });
+            connection.on('close', function (err) {
+                console.warn('MySQL close event', err);
+            });
+        });
+        resolve();
+    })
+}
+
+async function executeQuery(query) {
+    console.debug(`query: `, query)
+    return new Promise((resolve, reject) => {
+        try {
+            pool.query(query, (e, r, f) => {
+                if (e) {
+                    reject(e);
+                }
+                else {
+                    resolve(r);
+                }
+            });
+        }
+        catch (ex) {
+            reject(ex)
+        }
+    })
+}
+
+async function execSP(spName, params) {
+    return new Promise((resolve, reject) => {
+        try {
+            var paramPlaceHolder = ''
+            if (params && params.length) {
+                for (var i = 0; i < params.length; i++) {
+                    paramPlaceHolder += '?,'
+                }
+            }
+            if (paramPlaceHolder.length) {
+                paramPlaceHolder = paramPlaceHolder.slice(0, -1);
+            }
+            console.debug('final SP call', `CALL ${spName}(${params})`);
+            pool.query(`CALL ${spName}(${paramPlaceHolder})`, params, (e, r, f) => {
+                if (e) {
+                    reject(e);
+                }
+                else {
+                    resolve(r);
+                }
+            });
+        }
+        catch (ex) {
+            reject(ex);
+        }
+    })
+}
+module.exports.connect = connect;
+module.exports.executeQuery = executeQuery;
+module.exports.execSP = execSP;
